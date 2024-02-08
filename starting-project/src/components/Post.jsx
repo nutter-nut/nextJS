1a19ef38a62f65a5fd8b7bfd95904c96c24edef9
nutter kap <wongnaph1234@gmail.com>
2024-02-08 11:35:19 +0700

    update

--- starting-project/src/components/Post.jsx
+++ starting-project/src/components/Post.jsx
@@ -4,8 +4,7 @@ function Post(props){
     return ( 
         <li className={classes.post}>
             <p className={classes.author}>{props.author}</p>
-            <p className={classes.body}>{props.body}</p>
-            <p className={classes.name}>{props.name}</p>
+            <p className={classes.text}>{props.body}</p>
         </li>
     );
 }
--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,12 +1,15 @@
 import Post from './Post';
-import classes from './PostsList.module.css'
-
+import NewPost from './NewPost';
+import classes from './PostsList.module.css';
 function PostsList(){
     return (
-        <ul className={classes.posts}>
-            <Post author="Maximilion" body="React.js is awesome!" name="test" />
-            <Post author="Manuel" body="Check out the full course!" name="test2" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Maximilion" body="React.js is awesome!"/>
+                <Post author="Manuel" body="Check out the full course!" />
+            </ul>
+        </>
     );
 }
 
