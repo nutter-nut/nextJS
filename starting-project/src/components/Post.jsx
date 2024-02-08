d10f70e46b947300ee23845aa30ca6c1495a7d87
nutter kap <wongnaph1234@gmail.com>
2024-02-08 10:20:15 +0700

    update

---
+++ starting-project/src/components/Post.jsx
@@ -0,0 +1,13 @@
+import classes from './Post.module.css'
+
+function Post(props){
+    return (
+        <li className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.body}>{props.body}</p>
+            <p className={classes.name}>{props.name}</p>
+        </li>
+    );
+}
+
+export default Post;
---
+++ starting-project/src/components/PostsList.jsx
@@ -0,0 +1,13 @@
+import Post from './Post';
+import classes from './Post.module.css'
+
+function PostsList(){
+    return (
+        <ul className={classes.posts}>
+            <Post author="Maximilion" body="React.js is awesome!" name="test" />
+            <Post author="Manuel" body="Check out the full course!" name="test2" />
+        </ul>
+    );
+}
+
+export default PostsList;
